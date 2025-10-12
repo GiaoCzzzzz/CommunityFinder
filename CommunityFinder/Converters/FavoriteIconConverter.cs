6b4558230219c8462673b404ba0422749844a6a8
Gilfoyles-Anton <1921441793@qq.com>
2025-10-12 17:49:20 +0800

    Optimize the front end

---
+++ CommunityFinder/Converters/FavoriteIconConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace CommunityFinder.Converters
+{
+    public class FavoriteIconConverter : IValueConverter
+    {
+        // Converts IsFavorited(bool) → ❤️ 或 🤍
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isFav = value is bool b && b;
+            return isFav ? "❤️" : "🤍";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
---
+++ CommunityFinder/Converters/LikeIconConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace CommunityFinder.Converters
+{
+    public class LikeIconConverter : IValueConverter
+    {
+        // Converts IsLiked(bool) → Text emoji 👍 或 💚
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isLiked = value is bool b && b;
+            return isLiked ? "👍" : "👍🏻";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
