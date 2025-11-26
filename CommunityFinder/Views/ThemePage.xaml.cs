04a5030a01cb92d157fa8305cbca79c3602b8050
Gilfoyles-Anton <1921441793@qq.com>
2025-11-26 22:24:41 +0800

    Add the option to switch to the elderly mode

--- CommunityFinder/Views/ThemePage.xaml.cs
+++ CommunityFinder/Views/ThemePage.xaml.cs
@@ -1,9 +1,27 @@
+using Microsoft.Maui.Controls;
+
 namespace CommunityFinder.Views;
 
 public partial class ThemePage : ContentPage
 {
-	public ThemePage()
-	{
-		InitializeComponent();
-	}
+    public ThemePage()
+    {
+        InitializeComponent();
+        // Set initial mode text
+        CurrentModeLabel.Text = "Current Mode: Normal";
+    }
+
+    private void NormalModeButton_Clicked(object sender, EventArgs e)
+    {
+        // Set global font size to normal (12)
+        Application.Current.Resources["GlobalFontSize"] = 12.0;
+        CurrentModeLabel.Text = "Current Mode: Normal";
+    }
+
+    private void ElderModeButton_Clicked(object sender, EventArgs e)
+    {
+        // Set global font size to elder mode (16)
+        Application.Current.Resources["GlobalFontSize"] = 20.0;
+        CurrentModeLabel.Text = "Current Mode: Elder";
+    }
 }
