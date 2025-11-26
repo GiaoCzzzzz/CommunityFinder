c15a8ed61b1767cfd1b11ce02f371b12a1321454
Gilfoyles-Anton <1921441793@qq.com>
2025-11-26 20:33:07 +0800

    Fix the issue where the favorite icon fails to display.

--- CommunityFinder/Converters/FavoriteIconConverter.cs
+++ CommunityFinder/Converters/FavoriteIconConverter.cs
@@ -4,17 +4,30 @@ using Microsoft.Maui.Controls;
 
 namespace CommunityFinder.Converters
 {
+    /// <summary>
+    /// 将收藏状态 bool 转换为安卓/iOS 可显示的星星图标
+    /// </summary>
     public class FavoriteIconConverter : IValueConverter
     {
-        // Converts IsFavorited(bool) → ⭐ (yellow star) or ☆ (white star outline)
+        /// <summary>
+        /// 根据收藏状态返回星星字符（空心/实心）
+        /// </summary>
+        /// <param name="value">bool 类型，是否收藏</param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns>实心或空心星</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            bool isFav = value is bool b && b;
-            return isFav ? "🌟" : "⭐︎";
+            bool isFavorited = value is bool b && b;
+
+            // 安卓/iOS 都可以显示的 Unicode 星星
+            return isFavorited ? "🌟" : "⭐"; // 实心或空心星
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            => throw new NotImplementedException();
+        {
+            throw new NotImplementedException();
+        }
     }
 }
-    
